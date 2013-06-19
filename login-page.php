1232e1d51a38629b4bba004bf312c82613816ec8
Prathik Raj <prathik.raj@inmobi.com>
2013-06-19 21:29:20 +0530

    Added boilerplate based login page. Auto-redirect to Google authentication page.

--- login-page.php
+++ login-page.php
@@ -1,3 +1,37 @@
-<form action="?login" method="post">
-    <button>Login with Google</button>
-</form>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="boilerplate/css/normalize.css">
+        <link rel="stylesheet" href="boilerplate/css/main.css">
+        <script src="boilerplate/js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <h1>Inmobi</h1>
+	<p>Please wait while you are redirected to the login page.</p>
+	<p>Click the button below if you are not being redirected.</p>
+
+	<form id = "loginform" action="?login" method="post">
+    		<button>Login</button>
+	</form>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="boilerplate/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+        <script src="boilerplate/js/plugins.js"></script>
+        <script src="boilerplate/js/main.js"></script>
+    </body>
+</html>
